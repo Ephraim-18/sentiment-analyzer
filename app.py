2124a95641e7f4b9c1d4564233d162f84d560220
Ephraim-18 <ephraimsunny4@gmail.com>
2026-04-09 15:40:29 +0530

    fixed servers

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import joblib
 
 app = Flask(__name__)
 
-# load the model
 model = joblib.load("sentiment_model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
 
@@ -11,26 +10,26 @@ vectorizer = joblib.load("vectorizer.pkl")
 @app.route("/", methods=["GET", "POST"])
 def home():
     result = ""
+    confidence = 0
 
     if request.method == "POST":
         text = request.form["text"]
-        print("User input:", text)
+
         vector = vectorizer.transform([text])
         prediction = model.predict(vector)
-        probs = model.predict_proba([text])
-        confidence = max(probs[0])
-        confidence = round(confidence * 100, 2)
-        print("Prediction:", prediction)
+        probs = model.predict_proba(vector)
+
+        confidence = round(max(probs[0]) * 100, 2)
 
         if prediction[0] == 1:
-            result = f"Positive   ({confidence}%)"
+            result = "Positive"
         elif prediction[0] == 0:
-            result = f"Negative  ({confidence}%)"
+            result = "Negative"
         else:
-            result = f"Neutral  ({confidence}%)"
+            result = "Neutral"
 
     return render_template("index.html", result=result, confidence=confidence)
 
 
 if __name__ == "__main__":
-    app.run
+    app.run()
